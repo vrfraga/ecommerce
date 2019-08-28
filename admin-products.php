4d11d2f1758eea0dd48eca85cd05fde77fc5bcd8
valdirrodriguesger@gmail.com <valdirrodriguesger@gmail.com>
2019-08-28 18:14:42 -0300

    Admin Produtos - Inserir, Editar e Excluir

---
+++ admin-products.php
@@ -0,0 +1,104 @@
+<?php
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Product;
+
+$app->get("/admin/products", function() {
+    
+    User::verifyLogin();
+    
+    $products = Product::listAll();
+            
+    $page = new PageAdmin();
+    
+    $page->setTpl("products", [
+        "products"=>$products
+    ]);
+    
+});
+
+//rota para a tela de criação
+$app->get("/admin/products/create", function() {
+    
+    User::verifyLogin();
+                   
+    $page = new PageAdmin();
+    
+    $page->setTpl("products-create");
+    
+});
+
+//rota para inserir novo produto 
+$app->post("/admin/products/create", function() {
+    
+    User::verifyLogin();
+                   
+    $product = new Product();
+    
+    $product->setData($_POST);
+    
+    $product->save();
+        
+    header("Location: /admin/products");
+    exit;
+    
+});
+
+//rota para editar um produto 
+$app->get("/admin/products/:idproduct", function($idproduct) {
+    
+    User::verifyLogin();
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+                   
+    $page = new PageAdmin();
+    
+    $page->setTpl("products-update" , [
+        "product"=>$product->getValues()
+    ]);
+    
+});
+
+//rota para inserir uma foto nasta products
+$app->post("/admin/products/:idproduct", function($idproduct) {
+    
+    User::verifyLogin();
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+                   
+    $product->setData($_POST);
+    
+    $product->save();
+    
+    $product->setPhoto($_FILES["file"]);
+    
+    header('LOCATION: /admin/products');
+    exit;
+    
+});   
+
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct) {
+    
+    User::verifyLogin();
+    
+    $product = new Product();
+    
+    $product->get((int)$idproduct);
+
+    $product->delete();
+    
+    header('LOCATION: /admin/products');
+    exit;
+    
+    
+   
+});
+
+
+?>
