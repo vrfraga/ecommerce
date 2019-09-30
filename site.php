6de09236381d9b90f385fc63d5c56f54a98ac22d
valdirrodriguesger@gmail.com <valdirrodriguesger@gmail.com>
2019-09-30 17:28:38 -0300

    Paginação

--- site.php
+++ site.php
@@ -18,16 +18,31 @@ $app->get('/', function() {
 });
 
 $app->get("/categories/:idcategory", function($idcategory){
+    
+        $page = (isset($_GET['page'])) ?(int)$_GET['page'] : 1;
                
         $category = new Category();
         
         $category->get((int)$idcategory);
         
+        $pagination = $category->getProductsPage($page);
+        
+        $pages = [];
+        
+        for ($i=1; $i <= $pagination['pages']; $i++) {
+            array_push($pages, [
+                'link'=>'/category/'.$category->getidcategory().'?page='.$i,
+                'page'=>$i
+            ]);
+        }
+        
         $page = new Page();
     
         $page->setTpl("category", [
-            "category"=>$category->getValues(),
-            "products"=>$category->getProducts()
+            'category'=>$category->getValues(),
+            'products'=>$pagination["data"],
+            'pages'=>$pages
+                
         ]);
             
 });
