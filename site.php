dd7aa24a7b5de8da689b8a34d4a0a36fc26cf1df
valdirrodriguesger@gmail.com <valdirrodriguesger@gmail.com>
2019-12-13 22:28:34 -0300

    Site Cadastro

--- site.php
+++ site.php
@@ -178,7 +178,9 @@ $app->get("/login", function(){
     $page = new Page();
     
     $page->setTpl("login", [
-        'error'=>User::getError()
+        'error'=>User::getError(),
+        'errorRegister'=>User::getErrorRegister(),
+        'registerValues'=>(isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name'=> '', 'email'=> '', 'phone'=> '']
   ]); 
     
 });
@@ -213,12 +215,34 @@ $app->post("/register", function(){
     
     $_SESSION['registerValues'] = $_POST;
     
-    if(!isset($_POST['name']) || $_POST['name'] == '') {
+    if (!isset($_POST['name']) || $_POST['name'] == '') {
         
-       User::setErrorRegister("Preenncha o seu nome");
+       User::setErrorRegister("Preenncha o seu Nome");
        header("Location /login");
        exit; 
     }
+    
+    if (!isset($_POST['email']) || $_POST['email'] == '') {
+        
+       User::setErrorRegister("Preenncha o seu E-mail");
+       header("Location /login");
+       exit; 
+    }
+    
+     if (!isset($_POST['password']) || $_POST['password'] == '') {
+        
+       User::setErrorRegister("Preenncha o sua Senha");
+       header("Location /login");
+       exit; 
+    }
+    
+    if (User::checkLoginExist($_POST['email']) === true) {
+        
+           User::setErrorRegister("Este Endereço de E-mail Já Está Sendo Usado Por Outro Usuário");
+           header("Location /login");
+           exit;                 
+    }
+    
     
     $user = new User();
     
@@ -290,6 +314,32 @@ $app->get("logout", function(){
     User::logout();
     
     header("Location: /login");
+    
+    
+});
+
+
+$app->post("/register", function(){
+    
+    if (!isset($_POST['name']) $$ $_POST['name'] == '') {
+        
+        User::setErrorRegister("Preencha o seu nome");
+        header("Location /login");
+        exit;        
+    }
+    
+    $user = new User();
+    
+    $user->setData([ 
+        'inadmin'=>0,
+        'deslogin'=>$_POST['email'],
+        'desperson'=>$_POST['name'],
+        'desemail'=>$_POST['email'],
+        'despassword'=>$_POST['password'],
+        'nrphone'=>$_POST['phone']
+    ]);
+    
+    $user->save();
     
     
 });
