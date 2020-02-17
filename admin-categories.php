602eb9363862ae9be325b62bb9e51c087a1d0d58
valdirrodriguesger@gmail.com <valdirrodriguesger@gmail.com>
2020-02-17 17:45:41 -0300

    Admin - Categorias - Search and Pagination

--- admin-categories.php
+++ admin-categories.php
@@ -9,15 +9,43 @@ use \Hcode\Model\Product;
 
 $app->get("/admin/categories", function() {
            
-        User::verifyLogin();
-           
-        $categories = Category::listAll();
+    User::verifyLogin();
+        
+    $search = (isset($_GET['search']))? $_GET['search']: "";
+     
+     $page = (isset($_GET['page']))? (int)$_GET['page'] : 1;
+     
+     if ($search != ''){
+         
+         $pagination = Category::getPageSearch($search, $page);  
+         
+     } else {
+    
+         $pagination = Category::getPage($page);          
+         
+     }
+             
+     $pages = [];
+     
+     for ($x = 0; $x < $pagination['pages']; $x++)
+     {
+         array_push($pages,[ 
+            'href'=>'/admin/users?'.http_build_query([ 
+                'page'=>$x+1,
+                'search'=>$search
+            ]),     
+            'text'=>$x+1
+         ]);
+         
+     }          
           
+         
         $page = new PageAdmin();
           
-        $page->setTpl("categories", [
-               
-        "categories"=>$categories
+        $page->setTpl("categories", [               
+            "categories"=>$pagination['data'],
+            "search"=>$search,
+            "pages"=>$pages    
                
         ]);
            
