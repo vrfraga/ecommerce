e69b70684cea6e2d6352c3df16722b0811cbfb90
valdirrodriguesger@gmail.com <valdirrodriguesger@gmail.com>
2020-05-21 12:50:13 -0300

    Integração PayPal HTML

--- site.php
+++ site.php
@@ -284,13 +284,82 @@ $app->post("/checkout" , function(){
         
     ]);
     
-    $order->save();            
+    $order->save();     
     
-    header("Location: /order/".$order->getidorder());
+    switch ((int)$_POST['payment-method']){
+        
+        case 1:
+        header("Location: /order/".$order->getidorder()."/pagseguro");           
+        break;
+    
+        case 2:
+        header("Location: /order/".$order->getidorder()."/paypal");           
+        break;
+        
+    }
+    
     exit;
-            
+    
+});
+    
+    
+
+
+$app->get("/order/:idorder/pagseguro", function ($idorder){
+    
+    User::verifyLogin(false);
+    
+    $order = new Order();
+           
+    $order->get((int)$idorder);
+    
+    $cart = $order->getCart();
+    
+    $page = new Page([  
+        'header'=>false,
+        'footer'=>false
+        
+    ]);
+    
+    $page->setTpl("payment-pagseguro", [ 
+        'order'=>$order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getPrducts(),
+        'phone'=>[
+           'areaCode'=>substr($order->getnrphone(),0,2),
+           'number'=>substr($order->getnrphone(),2, strlen($order->getnrphone()))
+        ]
+    ]);   
+    
+   
 });
 
+
+$app->get("/order/:idorder/paypal", function ($idorder){
+    
+    User::verifyLogin(false);
+    
+    $order = new Order();
+           
+    $order->get((int)$idorder);
+    
+    $cart = $order->getCart();
+    
+    $page = new Page([  
+        'header'=>false,
+        'footer'=>false
+        
+    ]);
+    
+    $page->setTpl("payment-paypal", [ 
+        'order'=>$order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getPrducts(), 
+
+    ]);   
+    
+   
+});
 
 
 $app->get("/login", function(){    
--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -155,12 +155,12 @@ class Cart extends Model{
         $sql = new Sql();
         
         $rows = $sql->select("
-            SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllenght,b.vlweight,b.desurl COUNT(*) AS nrqtd, 
+            SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength,b.vlweight,b.desurl COUNT(*) AS nrqtd, 
             SUM(b.vlprice) AS vltotal
             FROM tb_cartsproducts a
             INNER JOIN tb_products b ON a.idproduct = b.idproduct
             WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
-            GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllenght,b.vlweight,b.desurl
+            GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength,b.vlweight,b.desurl
             ORDER BY b.desproduct
             ", [
                 ':idcart'=> $this->getidcart()
